067f05430ae0f7aed0c2843ee1a60065f60bfa92
jselga <jordi.selga@iescarelesvallbona.cat>
2024-04-16 23:54:10 +0200

    Initial Commit

---
+++ src/selgajordi_uf5_pt1/SelgaJordi_UF5_Pt1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package selgajordi_uf5_pt1;
+
+/**
+ *
+ * @author jordi
+ */
+public class SelgaJordi_UF5_Pt1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
