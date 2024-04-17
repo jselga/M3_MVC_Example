d3213e426d6927381889da3e4de963c091b5e3cb
jselga <jordi.selga@iescarelesvallbona.cat>
2024-04-17 18:00:30 +0200

    AddController added

---
+++ src/Controller/AddController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author jordi
+ */
+public class AddController {
+
+    MainController mainController;
+
+    public AddController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+}
